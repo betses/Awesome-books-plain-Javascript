40aa44daf732cc0b3e866e0aa805db41b85ab868
betse-1 <betsegaws@gmail.com>
2022-08-02 10:50:14 +0300

    add class

---
+++ README.md
@@ -0,0 +1,40 @@
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Portfolio
+Mobile version personal Portfolio page using css and html
+
+## Built With
+
+- HTML
+- CSS
+
+## Getting Started
+
+To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+  - none
+### Setup
+    - clone the repository 
+    - run Index.html file
+
+
+## Authors
+
+👤 **Author1**
+
+- GitHub: [@betses](https://github.com/betses)
+- Twitter: [@Betse_s](https://twitter.com/Betse_s)
+
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+
---
+++ main.js
@@ -0,0 +1,60 @@
+const collections = JSON.parse(localStorage.getItem('collections')) || [];
+const bookContainer = document.querySelector('.table');
+const AddBookForm = document.querySelector('.book-form');
+const bookTitle = document.getElementById('title');
+const bookAuthor = document.getElementById('author');
+
+
+class ViewBook {
+  constructor(book) {
+    this.book = book;
+  }
+
+  addBook = () => {
+    collections.push(this.book);
+    const myBook = collections;
+    localStorage.setItem('collections', JSON.stringify(myBook));
+    this.render();
+  }
+
+  render= () => {
+    bookContainer.innerHTML = '';
+    collections.forEach((book, index) => {
+      const bookElement = document.createElement('tr');
+      bookElement.classList.add('book');
+      bookElement.innerHTML = `
+          <td>${book.title}</td>
+          <td>${book.author}</td>
+          <td><button data-remove=${index} class='delete'>Remove</button></td>
+        `;
+      bookContainer.appendChild(bookElement);
+
+      const deleteButton = bookElement.querySelector('.delete');
+      deleteButton.addEventListener('click', () => {
+        this.removeBook(index);
+      });
+    });
+  }
+
+   removeBook = (index) => {
+     collections.splice(index, 1);
+     localStorage.setItem('collections', JSON.stringify(collections));
+     this.render();
+   }
+}
+
+const books = new ViewBook();
+
+AddBookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const book = {
+    title: bookTitle.value,
+    author: bookAuthor.value,
+  };
+  const books = new ViewBook(book);
+  books.addBook();
+  bookTitle.value = '';
+  bookAuthor.value = '';
+});
+window.onload = books.render();
