e18dd7083635e012b801fdd078a3e7b79193d286
niicode <niidarku66@gmail.com>
2022-08-03 08:12:38 +0000

    add page navigation

--- main.js
+++ main.js
@@ -4,6 +4,44 @@ const AddBookForm = document.querySelector('.book-form');
 const bookTitle = document.getElementById('title');
 const bookAuthor = document.getElementById('author');
 const error = document.querySelector('.error');
+const viewBook = document.getElementById('books-section');
+const addBook = document.getElementById('add-section');
+const contact = document.getElementById('contact-section');
+const list = document.getElementById('list');
+const addButton = document.getElementById('add-new');
+const contactButton = document.getElementById('contact');
+const formAdd = document.querySelector('.form-add');
+const isClicked = false;
+
+document.addEventListener('DOMContentLoaded', () => {
+  viewBook.style.display = 'none';
+  addBook.style.display = 'block';
+  contact.style.display = 'none';
+});
+
+list.addEventListener('click', () => {
+  if (isClicked === false) {
+    viewBook.style.display = 'block';
+    addBook.style.display = 'none';
+    contact.style.display = 'none';
+  }
+});
+
+addButton.addEventListener('click', () => {
+  if (isClicked === false) {
+    viewBook.style.display = 'none';
+    addBook.style.display = 'block';
+    contact.style.display = 'none';
+  }
+});
+
+contactButton.addEventListener('click', () => {
+  if (isClicked === false) {
+    viewBook.style.display = 'none';
+    addBook.style.display = 'none';
+    contact.style.display = 'block';
+  }
+});
 
 class Collections {
   constructor() {
@@ -77,9 +115,16 @@ AddBookForm.addEventListener('submit', (e) => {
   const author = bookAuthor.value;
   if (title === '' || author === '') {
     error.innerHTML = 'Please fill in all fields';
+    setTimeout(() => {
+      error.innerHTML = '';
+    }, 5000);
   } else {
     const books = new Book(title, author);
     books.addBook();
+    formAdd.innerHTML = `"${title}" was added to your collection`;
+    setTimeout(() => {
+      formAdd.innerHTML = '';
+    }, 5000);
     bookTitle.value = '';
     bookTitle.focus();
     bookAuthor.value = '';
